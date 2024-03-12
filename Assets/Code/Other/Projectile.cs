0ca11da5365950a00003860631dee01fbacfdea0
Nick <111998485+NicholasEng26@users.noreply.github.com>
2024-03-12 17:28:47 -0500

    Finished Q5 part 1
    - Just need to figure out how to activate animations

--- Assets/Code/Other/Projectile.cs
+++ Assets/Code/Other/Projectile.cs
@@ -2,39 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace SpaceShooter {
-    public class Projectile : MonoBehaviour {
+namespace SpaceShooter{
+    public class Projectile : MonoBehaviour
+    {
         // Outlets
         Rigidbody2D _rb;
 
         // State Tracking
+
         Transform target;
 
         // Methods
-        void Start() {
+        void Start()
+        {
             _rb = GetComponent<Rigidbody2D>();
-        }
-
-        void Update() {
-            // TODO: Make these dynanic
-            float acceleration = 1f;
-            float maxSpeed = 2f;
-
-            // Home in on target
-            ChooseNearestTarget();
-            if(target != null) {
-                // Rotate towards target
-                Vector2 directionToTarget = target.position - transform.position;
-                float angle = Mathf.Atan2(directionToTarget.y, directionToTarget.x) * Mathf.Rad2Deg;
-            
-                _rb.MoveRotation(angle);
-            }
-
-            // Accelerate forward
-            _rb.AddForce(transform.right * acceleration);
-
-            // Cap max speed
-            _rb.velocity = Vector2.ClampMagnitude(_rb.velocity, maxSpeed);
         }
 
         void ChooseNearestTarget() {
@@ -45,18 +26,18 @@ namespace SpaceShooter {
             Asteroid[] asteroids = FindObjectsOfType<Asteroid>();
 
             // Check each asteroid to see if it's the closest
-            for (int i = 0; i < asteroids.Length; i++) {
+            for(int i = 0; i < asteroids.Length; i++) {
                 Asteroid asteroid = asteroids[i];
 
                 // Asteroid must be to our right
                 if(asteroid.transform.position.x > transform.position.x) {
                     Vector2 directionToTarget = asteroid.transform.position - transform.position;
-                    
+
                     // Filter for the closest target we've seen so far
                     if(directionToTarget.sqrMagnitude < closestDistance) {
                         // Update closest distance for future comparisons
                         closestDistance = directionToTarget.sqrMagnitude;
-                        
+
                         // Track this asteroid as the current closest target
                         target = asteroid.transform;
                     }
@@ -64,9 +45,31 @@ namespace SpaceShooter {
             }
         }
 
+        void Update() {
+            // TODO: Make these dynamic
+            float acceleration = 1f;
+            float maxSpeed = 2f;
+
+            // Home in on target
+            ChooseNearestTarget();
+            if(target != null) {
+                // Rotate towards target
+                Vector2 directionToTarget = target.position - transform.position;
+                float angle = Mathf.Atan2(directionToTarget.y, directionToTarget.x) * Mathf.Rad2Deg;
+
+                _rb.MoveRotation(angle);
+            }
+
+            // Accelerate forward
+            _rb.AddForce(transform.right * acceleration);
+
+            // Cap max speed
+            _rb.velocity = Vector2.ClampMagnitude(_rb.velocity, maxSpeed);
+        }
+
         void OnCollisionEnter2D(Collision2D other) {
             // Only explode on Asteroids
-            if (other.gameObject.GetComponent<Asteroid>()) {
+            if(other.gameObject.GetComponent<Asteroid>()) {
                 Destroy(other.gameObject);
                 Destroy(gameObject);
 
@@ -79,6 +82,5 @@ namespace SpaceShooter {
                 Destroy(explosion, 0.25f);
             }
         }
-
     }
 }
