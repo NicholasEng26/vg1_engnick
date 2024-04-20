6435e0d3375e733001614a58a942d1d26b28a093
Nick <111998485+NicholasEng26@users.noreply.github.com>
2024-04-19 19:33:01 -0500

    Finished Q10

---
+++ Assets/Code/Adventure/Breakable.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Breakable : MonoBehaviour
+{
+    public void Break() {
+        Destroy(gameObject);
+    }
+}
--- Assets/Code/Adventure/PlayerController.cs
+++ Assets/Code/Adventure/PlayerController.cs
@@ -3,11 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 namespace Adventure {
+    public enum Direction {
+        Up = 0,
+        Down = 1,
+        Left = 2,
+        Right = 3
+    }
+
     public class PlayerController : MonoBehaviour
     {
         // Outlets
         Rigidbody2D _rigidbody;
         Animator _animator;
+        SpriteRenderer _spriteRenderer;
+        public Transform[] attackZones;
 
         // Configuration
         public KeyCode keyUp;
@@ -15,11 +24,16 @@ namespace Adventure {
         public KeyCode keyLeft;
         public KeyCode keyRight;
         public float moveSpeed;
+        public Sprite[] sprites;
+
+        // State Tracking
+        public Direction facingDirection;
 
         // Methods
         void Start() {
             _rigidbody = GetComponent<Rigidbody2D>();
             _animator = GetComponent<Animator>();
+            _spriteRenderer = GetComponent<SpriteRenderer>();
         }
 
         void Update() {
@@ -32,6 +46,33 @@ namespace Adventure {
 
             if(Input.GetKeyDown(KeyCode.Space)) {
                 _animator.SetTrigger("attack");
+
+                // Convert the enumeration to an index
+                int facingDirectionIndex = (int)facingDirection;
+
+                // Get an attack zone from index
+                Transform attackZone = attackZones[facingDirectionIndex];
+
+                // What objects are within a circle at that attack zone?
+                Collider2D[] hits = Physics2D.OverlapCircleAll(attackZone.position, 0.1f);
+
+                // Handle each hit target
+                foreach(Collider2D hit in hits) {
+                    Breakable breakableObject = hit.GetComponent<Breakable>();
+                    if(breakableObject) {
+                        breakableObject.Break();
+                    }
+                }
+
+            }
+        }
+
+        void LateUpdate() {
+            for(int i = 0; i < sprites.Length; i++) {
+                if(_spriteRenderer.sprite == sprites[i]) {
+                    facingDirection = (Direction)i;
+                    break;
+                }
             }
         }
 
